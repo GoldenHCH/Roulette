0e3809c8e840ba09257e0f97fb5a5b5e25d781bb
GoldenHCH <109107564+GoldenHCH@users.noreply.github.com>
2024-05-04 09:00:08 -0600

    finished game.py, started on combinations

--- combinations.py
+++ combinations.py
@@ -1,4 +1,12 @@
 from game import *
 
-amount = [i for i in range(1,101)]
-slot =
+amounts = [i for i in range(1,101)]
+slots = [int(i) for i in range(0,37)]+['even', 'odd', 'black', 'red', 'half1', 'half2', 'col1', 'col2', 'col3',
+                                        'doz1', 'doz2', 'doz3']
+cross = []
+
+for slot in slots:
+    for amount in amounts:
+        cross.append(bet(slot,amount))
+
+print(len(slots))
